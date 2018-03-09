c58480623103d60b1b2be68942f5fc9bf303dd4d
github@cybertekschool.com <github@cybertekschool.com>
2018-03-08 20:27:21 -0500

    removed Hello lines

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -3,9 +3,6 @@ package test;
 public class Hello {
 public static void main(String[] args) {
 	System.out.println("Hello Class!");
-	System.out.println("It is almost 5 pm!");
-	System.out.println("It is almost 5 pm!");
-	System.out.println("It is almost 5 pm!");
-	System.out.println("It is almost 5 pm!");
+	
 }
 }
