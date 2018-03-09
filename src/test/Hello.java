9a1e0d05aae0c9bfff3d3801495baf0edae149f9
github@cybertekschool.com <github@cybertekschool.com>
2018-03-08 20:33:21 -0500

    added array and loop

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -1,7 +1,17 @@
 package test;
 
+import java.lang.reflect.Array;
+
 public class Hello {
 public static void main(String[] args) {
+	
+	int a, b, c[];
+	
+	String home[] = {"Big", "Orange", "Tall", "Small"};
+	
+	for (String string : home) {
+		System.out.println("My home is: "+ string);
+	}
 	System.out.println("Hello Class!");
 	
 }
