eca2ba6ef940be89ecedb57d6b5e9e9df031feae
CybertekSchool <33201481+CybertekSchool@users.noreply.github.com>
2018-03-08 20:28:25 -0500

    Update Hello.java

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -3,6 +3,8 @@ package test;
 public class Hello {
 public static void main(String[] args) {
 	System.out.println("Hello Class!");
-	
+	for(int i=55; i<59; i++){
+	System.out.println("Hello Loop!");
+	}
 }
 }
